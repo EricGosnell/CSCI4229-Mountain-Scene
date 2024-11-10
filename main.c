9efdd4cbbabc971736a9fc9f271da0047a0a7433
Eric Gosnell <eric.gosnell@colorado.edu>
2024-11-09 18:17:40 -0700

    Refactored DEM loading and rendering system for performance.

--- main.c
+++ main.c
@@ -27,11 +27,18 @@ float fps = -1; // Starting value for FPS
 
 /* Digital Elevation Model */
 typedef struct {
-    float data[6178][6178]; // Elevation Data
+    float** data; // Elevation Data
     float pos[2]; // Top left corner UTM coordinate
 } DEM;
-DEM gore_range = {.pos[0] = 383807,
-				  .pos[1] = 4404735}; // 1 meter DEM starting at UTM 13 x383807 y4404735
+DEM gore_range = {.pos[0] = 0, // TODO: actual pos
+				  .pos[1] = 0}; // 1 meter DEM starting at UTM 13 x383807 y4404735
+typedef struct {
+    vtx A, B, C;
+    float rgba[4];
+    float normal[3];
+} DEM_triangle; // Triangle used in drawing the terrain
+DEM_triangle triangles[763848]; // 2*(6178/10)^2  TODO: dynamic allocation based on chunks
+
 float ymag = 1; // DEM vertical magnification
 
 /* First Person Camera Settings */
@@ -52,42 +59,6 @@ float shiny     = 1;    // Shininess (value)
 int l_th        = 90;   // Light azimuth
 float l_ph;    			// Elevation of light
 
-static void setColor(float slope_angle, float elevation) {
-    if (elevation < 3300) {
-        float forest[] = {0.13,0.55,0.13,1.0};
-        glColor4fv(forest);
-//        glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE,forest);
-    } else if (elevation < 3700) {
-        float tundra[] = {0.3,0.5,0.3,1.0};
-        glColor4fv(tundra);
-//        glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE,tundra);
-    } else {
-        float snow[] = {0.8,0.8,0.8,1.0};
-        glColor4fv(snow);
-//        glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE,snow);
-    }
-    if (slope_angle <= 0) {
-        float lake[] = {0.2,0.3,1,1.0};
-        glColor4fv(lake);
-//        glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE,lake);
-    }
-//    else if (slope_angle < 40) {
-//        float forest[] = {0.13,0.55,0.13,1.0};
-//        glColor4fv(forest);
-//        glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE,forest);
-//    }
-    else if (slope_angle > 40 && slope_angle < 50) {
-        float dirt[] = {0.5,0.4,0.3,1.0};
-        glColor4fv(dirt);
-//        glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE,dirt);
-    }
-    else if (slope_angle > 40 && slope_angle < 90) {
-        float stone[] = {0.25,0.25,0.2,1.0};
-        glColor4fv(stone);
-//        glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE,stone);
-    }
-}
-
 /*
  *  Draw vertex in polar coordinates with normal
  *  Credit: Vlakkies
@@ -101,31 +72,6 @@ static void Vertex(double th,double ph) {
    glNormal3d(x,y,z);
    glVertex3d(x,y,z);
 }
-
-/*
- * Draw a triangle
- */
-static void triangle(vtx A, vtx B, vtx C) {
-    //  Normal vector
-    float Nx = (A.y-B.y)*(C.z-A.z) - (C.y-A.y)*(A.z-B.z);
-    float Ny = (A.z-B.z)*(C.x-A.x) - (C.z-A.z)*(A.x-B.x);
-    float Nz = (A.x-B.x)*(C.y-A.y) - (C.x-A.x)*(A.y-B.y);
-    glNormal3f(Nx,Ny,Nz);
-    // Normalize the normal vector vertical component
-    float length = sqrt(Nx*Nx+Ny*Ny+Nz*Nz);
-    Ny /= length;
-    // Calculate slope angle
-    float slope_angle = acos(Ny) * (180/M_PI);
-    // Set color based on slope angle and elevation
-    setColor(slope_angle,A.y);
-    //  Draw triangle
-    glBegin(GL_TRIANGLES);
-        glVertex3f(C.x,C.y,C.z);
-        glVertex3f(B.x,B.y,B.z);
-        glVertex3f(A.x,A.y,A.z);
-    glEnd();
-    polygon_count++;
-};
 
 /*
  *  Draw a sphere
@@ -160,53 +106,64 @@ static void sphere(double x,double y,double z,double r, int inc) {
    glPopMatrix();
 }
 
-/*
- *  Draw 1 meter DEM Wireframe
- */
-void drawDEM(DEM* dem, double dx, double dy, double dz, double scale) {
-    const float dimension = sizeof(dem->data[0])/sizeof(float);
-    const int inc = 10; // Factor to reduce resolution by
-    // Save transformation
-    glPushMatrix();
-    // Translate and Scale
-    // TODO: I really don't know why these have to be backwards
-    glScaled(scale,scale,scale);
-    glTranslated(dx,dy,dz);
-
-    // Enable Face Culling
-    glEnable(GL_CULL_FACE);
-    // Set Color Properties
-    float black[]  = {0.0,0.0,0.0,1.0};
-    float white[]  = {1.0,1.0,1.0,1.0};
-    glMaterialfv(GL_FRONT,GL_EMISSION,black);
-    glMaterialfv(GL_FRONT,GL_SPECULAR,white);
-    glMaterialf(GL_FRONT,GL_SHININESS,shiny);
-    // Draw DEM Triangles
-	for (int i=0;i<dimension-inc;i+=inc) {
-		for (int j=0;j<dimension-inc;j+=inc) {
-            if (dem->data[i][j] == 0) continue;
-			float x = dem->pos[0] + i; // UTM x-coordinate
-			float z = dem->pos[1] - j; // UTM z-coordinate
-//            if (dem->data[i][j] > 3200) glColor3f(1,1,1);
-            vtx A = {x,ymag*dem->data[i][j],-z};
-            vtx B = {x+inc,ymag*dem->data[i+inc][j],-z};
-            vtx C = {x,ymag*dem->data[i][j+inc],-(z-inc)};
-            vtx D = {x+inc,ymag*dem->data[i+inc][j+inc],-(z-inc)};
-            triangle(A,B,C);
-            triangle(C,B,D);
-		}
+static void getColor(float slope_angle, DEM_triangle* tri) {
+    if (tri->A.y < 3300) {
+        float forest[] = {0.13,0.55,0.13,1.0};
+        tri->rgba[0] = forest[0];
+        tri->rgba[1] = forest[1];
+        tri->rgba[2] = forest[2];
+        tri->rgba[3] = forest[3];
+    } else if (tri->A.y < 3700) {
+        float tundra[] = {0.3,0.5,0.3,1.0};
+        tri->rgba[0] = tundra[0];
+        tri->rgba[1] = tundra[1];
+        tri->rgba[2] = tundra[2];
+        tri->rgba[3] = tundra[3];
+    } else {
+        float snow[] = {0.8,0.8,0.8,1.0};
+        tri->rgba[0] = snow[0];
+        tri->rgba[1] = snow[1];
+        tri->rgba[2] = snow[2];
+        tri->rgba[3] = snow[3];
     }
-    // Disable Face Culling
-    glDisable(GL_CULL_FACE);
-
-    // Undo transformations
-    glPopMatrix();
+    if (slope_angle <= 0) {
+        float lake[] = {0.2,0.3,1,1.0};
+        tri->rgba[0] = lake[0];
+        tri->rgba[1] = lake[1];
+        tri->rgba[2] = lake[2];
+        tri->rgba[3] = lake[3];
+    }
+    //    else if (slope_angle < 40) {
+    //        float forest[] = {0.13,0.55,0.13,1.0};
+    //        ...
+    //    }
+    else if (slope_angle > 40 && slope_angle < 50) {
+        float dirt[] = {0.5,0.4,0.3,1.0};
+        tri->rgba[0] = dirt[0];
+        tri->rgba[1] = dirt[1];
+        tri->rgba[2] = dirt[2];
+        tri->rgba[3] = dirt[3];
+    }
+    else if (slope_angle > 40 && slope_angle < 90) {
+        float stone[] = {0.25,0.25,0.2,1.0};
+        tri->rgba[0] = stone[0];
+        tri->rgba[1] = stone[1];
+        tri->rgba[2] = stone[2];
+        tri->rgba[3] = stone[3];
+    }
 }
 
 /*
  *  Read 1 meter DEM from the 4 corresonding files
  */
 void ReadDEM(char* file1, char* file2, char* file3, char* file4, DEM* dem) {
+   // Initialize data array
+   dem->data = malloc(6178 * sizeof(float*));
+   for (int i = 0; i < 6178; i++) {
+       dem->data[i] = malloc(6178 * sizeof(float));
+   }
+
+   // Read in data from files
    FILE* f1 = fopen(file1,"r");
    if (!f1) Fatal("Cannot open file %s\n",file1);
    FILE* f2 = fopen(file2,"r");
@@ -216,6 +173,7 @@ void ReadDEM(char* file1, char* file2, char* file3, char* file4, DEM* dem) {
    FILE* f4 = fopen(file4,"r");
    if (!f4) Fatal("Cannot open file %s\n",file4);
 
+   // Write elevation data for each point to the data array
    for (int i=0; i<6178; i++) {
        for (int j=0; j<6178; j++) {
            if (i<1544) {if (fscanf(f1,"%f",&dem->data[j][i])!=1) Fatal("Error reading %s\n", file1);}
@@ -229,6 +187,115 @@ void ReadDEM(char* file1, char* file2, char* file3, char* file4, DEM* dem) {
    fclose(f2);
    fclose(f3);
    fclose(f4);
+
+   /* Populate triangles array */ //TODO: different sized triangles based on distance to camera
+   const int inc = 10; // Factor by which to decrease resolution
+   int t = 6178/inc; // Width of triangles //TODO: dem->width
+
+   // Add elevation values from data
+   for (int i=0; i<t; i++) {
+       for (int j=0; j<t; j++) {
+           int n = (i*t+j)*2;
+           float x = dem->pos[0] + j*inc;
+           float z = dem->pos[1] - i*inc;
+           // Bottom left triangle
+           triangles[n].A.x = x;
+           triangles[n].A.y = dem->data[i*inc][j*inc];
+           triangles[n].A.z = z;
+
+           triangles[n].B.x = x;
+           triangles[n].B.y = dem->data[(i+1)*inc][j*inc];
+           triangles[n].B.z = z-inc;
+
+           triangles[n].C.x = x+inc;
+           triangles[n].C.y = dem->data[(i+1)*inc][(j+1)*inc];
+           triangles[n].C.z = z-inc;
+
+           // Top right triangle
+           triangles[n+1].A.x = x;
+           triangles[n+1].A.y = dem->data[i*inc][j*inc];
+           triangles[n+1].A.z = z;
+
+           triangles[n+1].B.x = x+inc;
+           triangles[n+1].B.y = dem->data[(i+1)*inc][(j+1)*inc];
+           triangles[n+1].B.z = z-inc;
+
+           triangles[n+1].C.x = x+inc;
+           triangles[n+1].C.y = dem->data[i*inc][(j+1)*inc];
+           triangles[n+1].C.z = z;
+
+           for (int k=0; k<2; k++) {
+               // Calculate normal vector
+               float Nx = (triangles[n+k].A.y-triangles[n+k].B.y)*(triangles[n+k].C.z-triangles[n+k].A.z) -
+               (triangles[n+k].C.y-triangles[n+k].A.y)*(triangles[n+k].A.z-triangles[n+k].B.z);
+               float Ny = (triangles[n+k].A.z-triangles[n+k].B.z)*(triangles[n+k].C.x-triangles[n+k].A.x) -
+               (triangles[n+k].C.z-triangles[n+k].A.z)*(triangles[n+k].A.x-triangles[n+k].B.x);
+               float Nz = (triangles[n+k].A.x-triangles[n+k].B.x)*(triangles[n+k].C.y-triangles[n+k].A.y) -
+               (triangles[n+k].C.x-triangles[n+k].A.x)*(triangles[n+k].A.y-triangles[n+k].B.y);
+               // Add normal vector to triangle
+               triangles[n+k].normal[0] = Nx;
+               triangles[n+k].normal[1] = Ny;
+               triangles[n+k].normal[2] = Nz;
+               // Normalize the normal vector vertical component
+               float length = sqrt(Nx*Nx+Ny*Ny+Nz*Nz);
+               Ny /= length;
+               // Calculate slope angle
+               float slope_angle = acos(Ny) * (180/M_PI);
+               // Set color based on slope angle and elevation
+               getColor(slope_angle,&triangles[n+k]);
+           }
+       }
+   }
+}
+
+/*
+ * Free dynamically allocated memory for the DEM
+ */
+void freeDEM(DEM* dem) {
+    for (int i = 0; i < 6178; i++) {
+        free(dem->data[i]);
+    }
+    free(dem->data);
+}
+
+/*
+ *  Draw 1 meter DEM Wireframe
+ */
+void drawDEM(double dx, double dy, double dz, double scale) {
+    // Save transformation
+    glPushMatrix();
+    // Translate and Scale
+    // TODO: I really don't know why these have to be backwards
+    glScaled(scale,scale,scale);
+    glTranslated(dx,dy,dz);
+
+
+    // Enable Face Culling
+    glEnable(GL_CULL_FACE);
+    // Set Color Properties
+    float black[]  = {0.0,0.0,0.0,1.0};
+    float white[]  = {1.0,1.0,1.0,1.0};
+    glMaterialfv(GL_FRONT,GL_EMISSION,black);
+    glMaterialfv(GL_FRONT,GL_SPECULAR,white);
+    glMaterialf(GL_FRONT,GL_SHININESS,shiny);
+    // Draw DEM Triangles
+    int nt = sizeof(triangles)/sizeof(DEM_triangle);
+    for (int i=0; i<nt; i++) {
+        glNormal3f(triangles[i].normal[0],triangles[i].normal[1],triangles[i].normal[2]);
+        glColor4f(triangles[i].rgba[0],triangles[i].rgba[1],triangles[i].rgba[2],triangles[i].rgba[3]);
+//        glMaterialfv(GL_FRONT,GL_AMBIENT_AND_DIFFUSE,triangles[i].rgba);
+        glBegin(GL_TRIANGLES);
+            glVertex3f(triangles[i].C.x, triangles[i].C.y, triangles[i].C.z);
+            glVertex3f(triangles[i].B.x, triangles[i].B.y, triangles[i].B.z);
+            glVertex3f(triangles[i].A.x, triangles[i].A.y, triangles[i].A.z);
+        glEnd();
+        polygon_count++;
+    }
+    // Disable Face Culling
+    glDisable(GL_CULL_FACE);
+
+    // Undo transformations
+    glPopMatrix();
 }
 
 
@@ -252,6 +319,7 @@ void display() {
 
     /* Set lighting values and create light source */
     glShadeModel(GL_SMOOTH);
+//    glShadeModel(GL_FLAT);
     //  Translate intensity to color vectors
     float Ambient[]   = {0.01*ambient ,0.01*ambient ,0.01*ambient ,1.0};
     float Diffuse[]   = {0.01*diffuse ,0.01*diffuse ,0.01*diffuse ,1.0};
@@ -279,8 +347,8 @@ void display() {
     glLightfv(GL_LIGHT0,GL_POSITION,Position);
 
     /* Draw Digital Elevation Models */
-    const int center = sizeof(gore_range.data[0])/sizeof(float)/2;
-    drawDEM(&gore_range,-gore_range.pos[0]-center,-3300*ymag,gore_range.pos[1]-center,1);
+    const int center = 6178/2; // TODO: gore_range.width
+    drawDEM(-gore_range.pos[0]-center,-3300*ymag,gore_range.pos[1]+center,1);
 
     /* Draw axes */
     glDisable(GL_LIGHTING);
@@ -369,6 +437,7 @@ void key(unsigned char ch,int x,int y) {
     switch (ch) {
         // ESC - Quit Program
         case 27:
+            freeDEM(&gore_range);
             exit(0);
         // Reset View Angle
         case '0':
@@ -526,5 +595,6 @@ int main(int argc,char* argv[]) {
     //  Pass control to GLUT so it can interact with the user
     ErrCheck("init");
     glutMainLoop();
+    freeDEM(&gore_range);
     return 0;
 }
